ef2ee049d71de8d9d8b2852de2ebbc0dc0eeb82a
chsohn15 <chsohn15@gmail.com>
2023-05-16 22:06:40 -0400

    tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :movie_locations
   resources :locations
   resources :productions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
